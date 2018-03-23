510d8c09a5d484bdb9988ad1c3787d64547bca73
mikeeus <itsmikias@gmail.com>
2018-03-23 22:34:45 +0300

    Add users controller, route and index action

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   resources :scores, only: [:index, :create]
+  get :scoreless, to: 'scores#scoreless'
+
+  resources :users, only: [:index]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   post :reverse, to: 'reverse#reverse', as: 'reverse'
