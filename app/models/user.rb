a78abdfbf05ce0cf2836549d7dd6d40e779c0a0c
mikeeus <itsmikias@gmail.com>
2018-03-23 22:34:25 +0300

    Add scoreless scope

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ApplicationRecord
   scope(:leaderboard, lambda {
     where('daily_score_count > 0').order('daily_score DESC')
   })
+
+  scope(:scoreless, lambda {
+    where('daily_score_count = 0')
+  })
 end
