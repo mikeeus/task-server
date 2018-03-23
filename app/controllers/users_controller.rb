6f6140ac3fd9cacfbd5fddc45a2c24e1012fc59d
mikeeus <itsmikias@gmail.com>
2018-03-24 01:14:21 +0300

    Serialize users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   before_action :set_user, only: :show
 
   def show
-    render json: { user: @user, scores: @user.scores }
+    render json: { user: ActiveModelSerializers::SerializableResource.new(@user), scores: @user.scores }
   end
 
   def index
